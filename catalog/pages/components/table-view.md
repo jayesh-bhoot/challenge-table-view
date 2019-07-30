c287e9f6e58153a02cc7f0df485afa6af9e0cf64
Jayesh Bhoot <jayesh@bhoot.sh>
2019-07-30 19:23:07 +0530

    feat: implement basic functionality of TableView

--- catalog/pages/components/table-view.md
+++ catalog/pages/components/table-view.md
@@ -1,3 +1,22 @@
 ``` react
-<TableView />
+state: {
+    filters: [
+        {key: 'genre', value: []},
+        {key: 'rating', value: []}
+    ]
+}
+---
+<TableView
+    rows={data}
+    columns={[
+        {label: 'Title', dataKey: 'title' },
+        {label: 'Genre', dataKey: 'genre' },
+        {label: 'Rating from 10', dataKey: 'rating'}
+    ]}
+    filters={state.filters}
+    onFilterChange={(key, value) => setState({
+        filters: state.filters.map(f => f.key === key
+            ? {'key': key, 'value': value}
+            : f)
+    })}/>
 ```
--- src/table-view/table-view.jsx
+++ src/table-view/table-view.jsx
@@ -2,22 +2,40 @@ import React from 'react'
 import createReactClass from 'create-react-class'
 import PropTypes from 'prop-types'
 
+import { default as Table } from '../table'
+import { default as Select } from '../select'
+
 export default createReactClass({
     displayName: 'TableView',
     propTypes: {
         rows: PropTypes.array,
         columns: PropTypes.array,
+        filters: PropTypes.array,
+        onFilterChange: PropTypes.func
         // classes: PropTypes.object.isRequired
     },
     getDefaultProps() {
         return {
             rows: [],
-            columns: []
+            columns: [],
+            filters: []
         }
     },
     render() {
-        return(
-            <div>Hello</div>
+        const { rows, columns, filters, onFilterChange } = this.props
+        return (
+            <div>
+                {filters.map(f => <Select
+                    multiple
+                    label={f.key}
+                    value={f.value}
+                    items={[... new Set(rows.map(r => r[f.key]))].sort()}
+                    onChange={e => onFilterChange(f.key, e.target.value)}
+                />)}
+                <Table
+                    rows={rows.filter(r => filters.every(f => !f.value.length || f.value.includes(r[f.key])))}
+                    columns={columns} />
+            </div>
         )
     }
 })
