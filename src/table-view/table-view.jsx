89bd6cbbb828fa0a0b44849c2025a903330f1e19
Jayesh Bhoot <jayesh@bhoot.sh>
2019-07-31 00:06:06 +0530

    test: write test cases for TableView component fix: fix filter bugs caught by the TableView tests

--- src/table-view/table-view.jsx
+++ src/table-view/table-view.jsx
@@ -7,50 +7,59 @@ import { default as Table } from '../table'
 import { default as Select } from '../select'
 
 export default createReactClass({
-    displayName: 'TableView',
-    propTypes: {
-        rows: PropTypes.array,
-        columns: PropTypes.array,
-        filters: PropTypes.array,
-        onFilterChange: PropTypes.func
-        // classes: PropTypes.object.isRequired
-    },
-    getDefaultProps() {
-        return {
-            rows: [],
-            columns: [],
-            filters: []
-        }
-    },
-    render() {
-        const { rows, columns, onFilterChange, onClearFilters } = this.props
-        const filters = this.props.filters.map(f => ({
-            ...f,
-            value: f.value || []
-        }))
-        const areFiltersEmpty = filters.every(f => !f.value.length)
-
-        return (
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <div style={{ display: 'flex' }}>
-                    {filters.map(f => <Select
-                        multiple
-                        label={f.label}
-                        value={f.value}
-                        items={[... new Set(rows.map(r => r[f.dataKey]))].sort()}
-                        onChange={e => onFilterChange(f.dataKey, e.target.value)}
-                    />)}
-                    {!areFiltersEmpty
-                        && <Button variant="contained" onClick={onClearFilters}>
-                            Clear Filters
-                    </Button>}
-                </div>
-                <Table
-                    rows={areFiltersEmpty
-                        ? rows
-                        : rows.filter(r => filters.every(f => f.value.includes(r[f.dataKey])))}
-                    columns={columns} />
-            </div>
-        )
+  displayName: 'TableView',
+  propTypes: {
+    rows: PropTypes.array,
+    columns: PropTypes.array,
+    filters: PropTypes.array,
+    onFilterChange: PropTypes.func,
+    onClearFilters: PropTypes.func
+  },
+  getDefaultProps () {
+    return {
+      rows: [],
+      columns: [],
+      filters: []
     }
+  },
+  render () {
+    const { rows, columns, onFilterChange, onClearFilters } = this.props
+    const filters = this.props.filters.map(f => ({
+      ...f,
+      value: f.value || []
+    }))
+    const areFiltersEmpty = filters.every(f => !f.value.length)
+
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div style={{ display: 'flex' }}>
+          {filters.map(f => (
+            <Select
+              key={f.dataKey}
+              multiple
+              label={f.label}
+              value={f.value}
+              items={[...new Set(rows.map(r => r[f.dataKey]))].sort()}
+              onChange={e => onFilterChange(f.dataKey, e.target.value)}
+            />
+          ))}
+
+          {!areFiltersEmpty &&
+          <Button variant='contained' onClick={onClearFilters}>
+                            Clear Filters
+          </Button>
+          }
+        </div>
+        <div>
+          <Table
+            rows={areFiltersEmpty
+              ? rows
+              : rows.filter(r => filters.every(f =>
+                !f.value.length ||
+                                f.value.includes(r[f.dataKey])))}
+            columns={columns} />
+        </div>
+      </div>
+    )
+  }
 })
